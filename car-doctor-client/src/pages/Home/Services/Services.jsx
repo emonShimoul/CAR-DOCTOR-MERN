38926d0f458e36537d77e9c9e366b646c3d8e4b4
emonShimoul <emonshimoul@gmail.com>
2023-06-03 01:51:43 +0600

    create checkout form and load single service data

--- car-doctor-client/src/pages/Home/Services/Services.jsx
+++ car-doctor-client/src/pages/Home/Services/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
   const [services, setServices] = useState([]);
 
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/services")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
@@ -20,7 +20,6 @@ const Services = () => {
           humour, or randomised <br /> words which do not look even slightly
           believable.{" "}
         </p>
-
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {services.map((service) => (
             <ServiceCard key={service._id} service={service}></ServiceCard>
