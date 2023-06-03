b8f134f41381c5e44fbea088d18afd83faf93373
emonShimoul <emonshimoul@gmail.com>
2023-06-03 18:05:29 +0600

    display bookings and service image in a tabular format

--- car-doctor-client/src/pages/Bookings/Bookings.jsx
+++ car-doctor-client/src/pages/Bookings/Bookings.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import BookingRow from "./BookingRow";
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
@@ -15,6 +16,30 @@ const Bookings = () => {
   return (
     <div>
       <h2 className="text-5xl">Your bookings: {bookings.length}</h2>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Image</th>
+              <th>Service</th>
+              <th>Date</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <BookingRow key={booking._id} booking={booking}></BookingRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
