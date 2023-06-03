becc2e97b5e702c229162c0810b474ce89cc81de
emonShimoul <emonshimoul@gmail.com>
2023-06-03 17:20:55 +0600

    save order on the order collection using post method

--- car-doctor-client/src/pages/BookService/BookService.jsx
+++ car-doctor-client/src/pages/BookService/BookService.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from "../../providers/AuthProvider";
 
 const BookService = () => {
   const service = useLoaderData();
-  const { title, _id, price } = service;
+  const { title, _id, price, img } = service;
   const { user } = useContext(AuthContext);
 
   const handleBookService = (e) => {
@@ -15,15 +15,32 @@ const BookService = () => {
     const date = form.date.value;
     const email = user?.email;
 
-    const order = {
+    const booking = {
       customerName: name,
       email,
+      img,
       date,
-      service: _id,
+      service_id: _id,
+      service: title,
       price,
     };
 
-    console.log(order);
+    console.log(booking);
+
+    fetch("http://localhost:5000/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("Service booked successfully!!");
+        }
+      });
   };
 
   return (
--- car-doctor-client/src/pages/Shared/NavBar/NavBar.jsx
+++ car-doctor-client/src/pages/Shared/NavBar/NavBar.jsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.svg";
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const NavBar = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.log(error));
+  };
+
   const navItems = (
     <>
       <li>
@@ -10,6 +20,17 @@ const NavBar = () => {
       <li>
         <Link to="/about">About</Link>
       </li>
+      {user?.email ? (
+        <li>
+          <Link onClick={handleLogOut} to="/">
+            Log Out
+          </Link>
+        </li>
+      ) : (
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      )}
     </>
   );
   return (
